2b2173ac19fe6e5b26d65e5cc271df21218861d2
luktvpl <luktvpl@wp.pl>
2021-10-19 18:42:40 +0200

    beta 1.6

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "minecraft bedrock za darmo", image: "images/index/bedrock.jpg", link:"#" },
+  { name: "minecraft bedrock za darmo", image: "images/index/bedrock.jpg", link:"subsite/freemc.html" },
   { name: "tarcza lisek", image: "images/zegarek/fox.webp", link:"#" },
   { name: "serwery java na bedrocku", image: "images/index/java-bedrock.png", link:"#" },
   { name: "Polskie SMP", image: "images/index/polskie_smp.png", link:"#" },
   { name: "Addon", image: "images/index/addon.png", link:"#" },
-  { name: "pusty", image: "", link:"#" },
-  { name: "pusty", image: "", link:"#" },
   { name: "pasjans (google)", image: "images/index/pasjans.png", link:"pasjans/index.html" },
-  { name: "test", image: "images/index/test.webp", link:"#" },
+  { name: "test", image: "images/index/test.webp", link:"subsite/template.html" },
 ]
 const showCoffees = () => {
     let output = ""
