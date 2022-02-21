735ac9db5b7051d890dbc22b730b7648b68451be
Your Name <luktvpl@wp.pl>
2022-02-21 15:11:23 +0100

    luknet integracja

--- js/app.js
+++ js/app.js
@@ -63,11 +63,61 @@ function getCookie(cname) {
 
 const ws = new WebSocket("wss://luknet.duckdns.org:3306");
 
+
+
+//konta
+try {
+var st = document.getElementById("st")
+var space = document.getElementById("acc");
+}catch{
+
+}
+st.innerHTML = '<b>status : offline</b>';
 ws.addEventListener("open", () => {
+  st.innerHTML = '<p><b>status : online</b></p>';
   console.log("połączono")
-  ws.send("witam");
+  ws.send("i:acc");
   ws.addEventListener("message", msg => {
-    alert(msg.data);
+    var command = msg.data.split(":")
+    if(msg.data="g:id"){
+      if(getCookie("reg")=="y"){
+      ws.send("id:"+getCookie("uuid"))
+    }else{
+      ws.send("c:id");
+      
+    }
+    }
+    if(command[0]=="id"){
+        alert("dostałęś własne id");
+        setCookie("reg","y",3600)
+        setCookie("uuid",command[1],3600)
+    }
+    
+  })
+  ws.addEventListener("close", lol => {
+    st.innerHTML = '<b>status : offline</b>';
   })
 })
 
+//cookie
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
