7b6547110afe00eee63b42397ac44bdf8a740ebd
Your Name <luktvpl@wp.pl>
2022-04-22 08:46:18 +0200

    fox team + blacklist update

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const container = document.querySelector(".pojemnik")
 const coffees = [
   { name: "Moje kanały", image: "/obrazki/miniaturki/my_chanals.webp", link: "/podstrony/my_chanals.html",nt:true },
   //{ name: "Beta RPG", image: "/obrazki/miniaturki/rpg.webp", link: "/beta/index.html",nt:false },
-  { name: "PngLuk", image: "/obrazki/miniaturki/foxteam.webp", link: "/podstrony/foxteam.html",nt:false },
+  { name: "Fox Team", image: "/obrazki/miniaturki/foxteam.webp", link: "/podstrony/foxteam.html",nt:false },
   { name: "PngLuk", image: "/obrazki/miniaturki/pngluk.webp", link: "https://luktvpl.github.io/PNGluk/",nt:false },
   { name: "Animowany Luk", image: "/obrazki/miniaturki/luk_kukła.webp", link: "/podstrony/luk_kukla.html",nt:false },
   { name: "Czarna lista", image: "/obrazki/miniaturki/black_list.webp", link: "/podstrony/black_list.html",nt:false }
