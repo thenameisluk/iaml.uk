cb360f235abb591c7a8854d7c8943bd04b9a2d67
luktvpl <92756992+luktvpl@users.noreply.github.com>
2022-05-22 17:31:13 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,15 +2,10 @@
 /*wkrótce*/
 
 const container = document.querySelector(".pojemnik")
-const coffees = [
-  { name: "Moje kanały", image: "/obrazki/miniaturki/my_chanals.webp", link: "/podstrony/my_chanals.html",nt:false },
-  //{ name: "Beta RPG", image: "/obrazki/miniaturki/rpg.webp", link: "/beta/index.html",nt:false },
-  { name: "Fox Team", image: "/obrazki/miniaturki/foxteam.webp", link: "/podstrony/foxteam.html",nt:false },
-  { name: "luk pack", image: "/obrazki/miniaturki/pack.png", link: "/podstrony/luk_pack.html",nt:false },
-  { name: "PngLuk", image: "/obrazki/miniaturki/pngluk.webp", link: "https://luktvpl.github.io/PNGluk/",nt:true },
-  { name: "Animowany Luk", image: "/obrazki/miniaturki/luk_kukła.webp", link: "/podstrony/luk_kukla.html",nt:false },
-  { name: "Czarna lista", image: "/obrazki/miniaturki/black_list.webp", link: "/podstrony/black_list.html",nt:false }
-]
+fetch('https://luktvpl.github.io/Dokumenty/Cheaty.json')
+  .then(response => response.json())
+  .then(data => {
+const coffees = data.ps
 
 const showCoffees = () => {
   let output = ""
@@ -38,7 +33,7 @@ const showCoffees = () => {
   )
   container.innerHTML = output
 
-}
+}});
 //caches.keys().then(c=>c.forEach(cc=>caches.delete(cc)))
 document.addEventListener("DOMContentLoaded", showCoffees)
 if ("serviceWorker" in navigator) {
