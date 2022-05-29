879af8481615ae92bc1720ce41887e1e214ef167
Your Name <luktvpl@wp.pl>
2022-05-29 14:49:42 +0200

    więcej

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+var cards = {};
+
 var conten = document.getElementById("fullcon")
 var win = document.getElementById("win")
 var okno = '<div class="topw">test <img src="./obrazki/ui/x.png" class="x" id="x"/> </div>'
 var blure ="-webkit-filter: blur(5px);-moz-filter: blur(5px);-o-filter: blur(5px);-ms-filter: blur(5px);filter: blur(5px);"
+var mottol = document.getElementById("motto")
 function test() {
 conten.style = blure
 win.innerHTML = okno.toString()
@@ -10,4 +13,19 @@ document.getElementById("x") .addEventListener("click", i => {
 conten.style=""
 win.innerHTML = ""
 })
+}
+var process = 1;
+var size = 20
+setInterval(() => {
+    if(process==1)size+=1;
+    else size-=1;
+    if(size>25)process=0
+    if(size<20)process=1
+    mottol.style = "font-size:"+size+"px;"
+}, 128);
+function getinnersite(name){
+
+}
+function getItem(imgName,Title,innersite){
+
 }
---
+++ js/undersite_functions.js
@@ -0,0 +1,3 @@
+function black_list(){
+    var bl = document.getElementById("black_list")
+}
