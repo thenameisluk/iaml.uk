4f6e08bf6efcc505f351bfec52e74959e59a82a1
lukishere <lukasz.osadik@wp.pl>
2023-08-08 18:32:48 +0200

    preview 3

--- projects.js
+++ projects.js
@@ -1,11 +1,30 @@
 var projects = [
     {
         title:"OwO",
+        image:"dsl.png",
+        url:"./index.html"
+    },
+    {
+        title:"Dark Os",
         image:"do.png",
-        url:"https://google.pl"
+        url:"./index.html"
     }
 ]
 
+var project = 0;
+
+
+
 function nextProj(){
-    console.log("hi")   
+    console.log("hi")
+    var title = document.getElementById("proj-title")
+    var img = document.getElementById("proj-img")
+    var link = document.getElementById("proj-link")
+
+    project++;
+    if(project>=projects.length)
+        project=0;
+    title.innerHTML = projects[project].title
+    img.setAttribute("src","./obrazki/proj/"+projects[project].image)
+    link.setAttribute("href",projects[project].url)
 }
